ba3b9e4b4524636adda15ad3c9a1c9a094f35b7d
Malcolm Stone <malcolm.stone@geeks.ltd.uk>
2016-03-07 17:27:17 +0000

    Move swapi related logic to folder

---
+++ src/starwars/swapi/swapi-service.ts
@@ -0,0 +1,22 @@
+import {SwapiListResponse} from "./swapi-list-response";
+import {Planet} from "../planets/planet";
+import {Person} from "../people/person";
+import {Starship} from "../starships/starship";
+
+declare function fetch(url: string): any;
+
+function fetchResource<T>(url: string): PromiseLike<SwapiListResponse<T>> {
+  return fetch(url).then(res => res.json());
+}
+
+export function fetchPlanets(url = "http://swapi.co/api/planets") {
+  return fetchResource<Planet>(url);
+}
+
+export function fetchPeople(url = "http://swapi.co/api/people") {
+  return fetchResource<Person>(url);
+}
+
+export function fetchStarships(url = "http://swapi.co/api/starships") {
+  return fetchResource<Starship>(url);
+}
