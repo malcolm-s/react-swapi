d4f032d4c8e72f26905a5342db866178c4d1cfc2
Malcolm Stone <malcolm.stone@geeks.ltd.uk>
2016-03-15 18:11:20 +0000

    remove unused methods from swapi-service

--- src/starwars/swapi/swapi-service.ts
+++ src/starwars/swapi/swapi-service.ts
@@ -11,27 +11,3 @@ declare function fetch(url: string): any;
 export function fetchResource<T>(url: string): PromiseLike<SwapiListResponse<T>> {
   return fetch(url).then(res => res.json());
 }
-
-export function fetchPlanets(url = "http://swapi.co/api/planets") {
-  return fetchResource<Planet>(url);
-}
-
-export function fetchPeople(url = "http://swapi.co/api/people") {
-  return fetchResource<Person>(url);
-}
-
-export function fetchStarships(url = "http://swapi.co/api/starships") {
-  return fetchResource<Starship>(url);
-}
-
-export function fetchSpecies(url = "http://swapi.co/api/species") {
-  return fetchResource<Species>(url);
-}
-
-export function fetchVehicles(url = "http://swapi.co/api/vehicles") {
-  return fetchResource<Vehicle>(url);
-}
-
-export function fetchFilms(url = "http://swapi.co/api/films") {
-  return fetchResource<Film>(url);
-}
